c3ee1e071318f6d60368575c35e01620843235b3
slashwagner <slashem634@gmail.com>
2026-08-21 19:47:56 +0300

    OSINT Tool v3.0 - 4 рабочие функции

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function drawStars() {
 }
 drawStars();
 
-// ===== ВКЛАДКИ =====
+// ===== ПЕРЕКЛЮЧЕНИЕ ВКЛАДОК =====
 document.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('click', function () {
         document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
